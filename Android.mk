0083709a8f15bdc4b7d484d7a81ff643218e8979
youling257 <youling257@gmail.com>
2022-04-28 03:54:20 +0800

    update Android.mk and remove test

--- Android.mk
+++ Android.mk
@@ -79,21 +79,8 @@ LOCAL_CPPFLAGS += \
 	-DHWC2_USE_CPP11 \
 	-DHWC2_INCLUDE_STRINGIFICATION
 
-
-ifeq ($(TARGET_PRODUCT),hikey960)
-LOCAL_CPPFLAGS += -DUSE_HISI_IMPORTER
-LOCAL_SRC_FILES += platformhisi.cpp
-LOCAL_C_INCLUDES += device/linaro/hikey/gralloc960/
-else ifeq ($(TARGET_PRODUCT),hikey)
-LOCAL_CPPFLAGS += -DUSE_HISI_IMPORTER
-LOCAL_SRC_FILES += platformhisi.cpp
-LOCAL_C_INCLUDES += device/linaro/hikey/gralloc/
-else ifeq ($(TARGET_BOARD_PLATFORM),android-x86)
 LOCAL_SRC_FILES += platformminigbm.cpp
 LOCAL_C_INCLUDES += external/minigbm/cros_gralloc/
-else
-LOCAL_CPPFLAGS += -DUSE_DRM_GENERIC_IMPORTER
-endif
 
 LOCAL_MODULE := hwcomposer.minigbm
 LOCAL_MODULE_TAGS := optional
